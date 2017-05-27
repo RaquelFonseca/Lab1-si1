90be243d7ff8ced2ab62725154d2d1fed39bd1d6
Raquel Ambrozio <raquel.fonseca@ccc.ufcg.edu.br>
2017-05-27 17:02:31 -0300

    funcionalidade de encontrar o minimo adicionada

--- programa.py
+++ programa.py
@@ -8,3 +8,14 @@ def encontra_maximo(array):
 
 
 assert encontra_maximo([27, 5, 2017]) == 2017
+
+def encontra_minimo(array):
+    minimo = array[0]
+    for i in array:
+        if minimo > i:
+            minimo = i
+
+    return minimo
+
+
+assert encontra_minimo([27, 5, 2017]) == 5
